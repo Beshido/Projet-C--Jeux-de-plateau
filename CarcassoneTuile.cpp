c24a1e088de97c195a49f7258f0df184837672aa
lejeunea <albanlejeune94@gmail.com>
2022-12-27 15:25:46 +0100

    Avion

--- CarcassoneTuile.cpp
+++ CarcassoneTuile.cpp
@@ -18,3 +18,20 @@ CarcassoneTuile::CarcassoneTuile(CarcassoneType typeNord, CarcassoneType typeOue
     typeEst,
     typeSud
 } {}
+bool IsSurrounded() const {
+    // Check if there are tiles in all four directions
+    return north_ != nullptr && south_ != nullptr &&
+           east_ != nullptr && west_ != nullptr;
+  }
+TileType GetType() const {
+    return type_;
+  }
+bool HasFollower() const {
+    return has_follower_;
+  }
+  // Returns true if the tile is part of a completed area, false otherwise
+bool IsCompleted() const {
+    // Check if the tile is a city or road and if it is fully surrounded by other tiles
+    return (type_ == CarcassoneType::City || type_ == CarcassoneTuile::Road) &&
+           IsSurrounded();
+  }
